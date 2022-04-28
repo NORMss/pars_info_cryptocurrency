f915796bdec7e09bfed80ffafacb9bc0515732af
Сергей Безбородов <serega.b301@gmail.com>
2022-04-29 01:59:22 +0700

    created: getting information from all columns

--- main.py
+++ main.py
@@ -14,14 +14,49 @@ def get_html(url, params=None):
 
 def get_content(html):
     soup = BeautifulSoup(html.text, 'html.parser')
-    items = soup.find_all('tr')
+    blocks = soup.find_all('tr')
     cryptocurrency=[]
-    for item in items:
-        items2=item.find_all('p',class_ = 'sc-1eb5slv-0 iworPT')
-        for item2 in items2:
+    for block in blocks:
+        items_name=block.find_all('p',class_ = 'sc-1eb5slv-0 iworPT')
+        items_prise=block.find_all('div',class_ = 'sc-131di3y-0 cLgOOr')
+        items_percent24h=block.find_all('span',class_='sc-15yy2pl-0 kAXKAX')
+        items_percent7d=block.find_all('span',class_='sc-15yy2pl-0 hzgCfk')
+        items_marketcap=block.find_all('span',class_='sc-1ow4cwt-0 iosgXe')
+        items_volume24h=block.find_all('p',class_='sc-1ow4cwt-0 iosgXe')
+        items_circulsupp=block.find_all('p',class_='sc-1eb5slv-0 kZlTnE')
+        for item_name in items_name:
             cryptocurrency.append({
-                'name': item2.text
+                'name': item_name.text
             })
+        for item_prise in items_prise:
+            cryptocurrency.append({
+                'prise': item_prise.text
+            })
+        for item_percent24h in items_percent24h:
+            cryptocurrency.append({
+                'percent24h': item_percent24h.text
+            })
+        for item_percent7d in items_percent7d:
+            cryptocurrency.append({
+                'percent7d': item_percent7d.text
+            })
+        for item_marketcap in items_marketcap:
+            cryptocurrency.append({
+                'marketcap': item_marketcap.text
+            })
+        for item_marketcap in items_marketcap:
+            cryptocurrency.append({
+                'marketcap': item_marketcap.text
+            })
+        for item_volume24h in items_volume24h:
+            cryptocurrency.append({
+                'volume24h': item_volume24h.text
+            })
+        for item_circulsupp in items_circulsupp:
+            cryptocurrency.append({
+                'circulsupp': item_circulsupp.text
+            })
+        
     print(cryptocurrency)
 
 def parse():
